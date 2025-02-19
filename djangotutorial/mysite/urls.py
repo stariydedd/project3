85b4cac054c4a0a03072c64c5dfb60a1296c66a8
stariydedd <94466840+stariydedd@users.noreply.github.com>
2025-02-19 08:40:29 +0000

    zanyatie_4

--- djangotutorial/mysite/urls.py
+++ djangotutorial/mysite/urls.py
@@ -18,7 +18,31 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
+from django.urls import path, include
+from polls.models import Question
+from rest_framework import routers, serializers, viewsets
+
+# Serializers define the API representation.
+class QuestionSerializer(serializers.HyperlinkedModelSerializer):
+    class Meta:
+        model = Question
+        fields = ['question_text', 'pub_date']
+
+# ViewSets define the view behavior.
+class QuestionViewSet(viewsets.ModelViewSet):
+    queryset = Question.objects.all()
+    serializer_class = QuestionSerializer
+
+# Routers provide an easy way of automatically determining the URL conf.
+router = routers.DefaultRouter()
+router.register(r'Question', QuestionViewSet)
+
+# Wire up our API using automatic URL routing.
+# Additionally, we include login URLs for the browsable API.
+
 urlpatterns = [
     path("polls/", include("polls.urls")),
     path("admin/", admin.site.urls),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
